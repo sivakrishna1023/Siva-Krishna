1e5b391880dc05d71a013ba27e5045e5920dcdcc
Siva Krishna <119127579+sivakrishna1023@users.noreply.github.com>
2025-09-15 01:24:10 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -438,7 +438,7 @@ function App() {
 
           </div>
           <a
-            href="https://drive.google.com/file/d/1oysaYgVo8MZg1YolO7QloYTTvTiOIcqO/view"
+            href="https://drive.google.com/file/d/1S1Iyrgfhj8yybCtzJDT886WnaGzJ9QGb/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block mt-6 px-6 py-2 bg-gray-400 text-white font-semibold rounded-lg hover:bg-blue-500 dark:bg-gray-400 dark:hover:bg-blue-500 transition-colors duration-300"
