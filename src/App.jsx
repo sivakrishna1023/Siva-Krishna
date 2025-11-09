8f1d6b55b785326a330fc43cae956b702b96ad0e
Siva Krishna <119127579+sivakrishna1023@users.noreply.github.com>
2025-11-09 16:52:26 +0530

    Fix syntax errors in job descriptions

--- src/App.jsx
+++ src/App.jsx
@@ -219,10 +219,9 @@ function App() {
       role:"Trainee Software Engineer",
       date:"August 2025 -- Present",
       description:[
-       "Translated large volumes of business data into actionable insights for top clients (YouTube, Google Ads) using advanced, high-performance SQL queries."
-      "Automated manual database-table access authorizations by implementing SQL-driven workflows and scripts."
-      "Built an ETL pipeline using Java functions for data ingestion and SQL for joins/materialized tables to consolidate multi-source data into a dashboard, cutting report time from ∼1.5 hrs to ∼5 mins."
- 
+       "Translated large volumes of business data into actionable insights for top clients (YouTube, Google Ads) using advanced, high-performance SQL queries.";
+      "Automated manual database-table access authorizations by implementing SQL-driven workflows and scripts.",
+      "Built an ETL pipeline using Java functions for data ingestion and SQL for joins/materialized tables to consolidate multi-source data into a dashboard, cutting report time from ∼1.5 hrs to ∼5 mins.",
       ]
     },
     {
@@ -232,8 +231,8 @@ function App() {
       description:[
         "Developed REST APIs for user registration, login, and user profile management, incorporating various CRUD operations for enhanced user experience.",
         "Implemented secure reset and forgot-password workflows with OTP (email/SMS) authentication and a token-bucket rate limiter (1 request/10s), enforcing API-level throttling for DDoS mitigation and automated-abuse prevention.",
-        "Developed a seamless refund processing system by integrating backend services with payment-gateway APIs to enable secure, reliable refund handling."
-        "Built GitHub Actions CI/CD pipelines to automate build/test/deploy from Git repos to Cloud, cutting manual deployment time by 90% and reducing human error."
+        "Developed a seamless refund processing system by integrating backend services with payment-gateway APIs to enable secure, reliable refund handling.".
+        "Built GitHub Actions CI/CD pipelines to automate build/test/deploy from Git repos to Cloud, cutting manual deployment time by 90% and reducing human error.",
       ]
     },
     {
